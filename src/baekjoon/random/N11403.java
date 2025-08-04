f264dd9e8d269b2011fa3c1be1971088dabb9ac8
정윤환 <629jyh7@naver.com>
2025-08-04 16:53:13 +0900

    feat: 백준 문제(플로이드-워셜 -> dfs) 풀이 추가

--- src/baekjoon/random/N11403.java
+++ src/baekjoon/random/N11403.java
@@ -6,17 +6,21 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class N11403 {
+    static boolean[] visit;
+    static int[][] arr;
+    static int N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n][n];
-        for(int i=0; i<n; i++){
+        arr = new int[N][N];
+        for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++){
+            for(int j=0; j<N; j++){
                 int num = Integer.parseInt(st.nextToken());
                 if(i == j) continue;
                 if(num == 1){
@@ -31,9 +35,9 @@ public class N11403 {
 
             쉽게 말해서 i -> k(중간 노드) -> j를 통해서 경로에 갈 수 있는지 판단
          */
-        for(int k=0; k<n; k++){
-            for(int i=0; i<n; i++){
-                for(int j=0; j<n; j++){
+        for(int k=0; k<N; k++){
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
                     if(arr[i][k] == 1 && arr[k][j] == 1){
                         arr[i][j] = 1;
                     }
@@ -41,11 +45,26 @@ public class N11403 {
             }
         }
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+//        for(int i=0; i<N; i++){
+//            visit = new boolean[N];
+//            dfs(i, i);
+//        }
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
+        }
+    }
+
+    public static void dfs(int start, int current){
+        for(int next=0; next<N; next++){
+            if(!visit[next] && arr[current][next] == 1){
+                arr[start][next] = 1;
+                visit[next] = true;
+                dfs(start, next);
+            }
         }
     }
 }
