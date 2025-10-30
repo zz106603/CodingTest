460f3584e460d822e07361ef8899cb59f4f31561
YUNHWAN <629jyh7@naver.com>
2025-10-30 21:07:22 +0900

    add: 백준 문제(정렬, 우선순위큐) 풀이 추가

--- src/baekjoon/kt/N19598.java
+++ src/baekjoon/kt/N19598.java
@@ -4,81 +4,53 @@ import java.io.*;
 import java.util.*;
 
 public class N19598 {
-    static class Room {
-        int start;
-        int end;
-        boolean used;
-        Room(int start, int end, boolean used){this.start=start; this.end=end; this.used=used;}
-    }
-
-    static boolean check(Room[] rooms){
-        for(Room r : rooms){
-            if(!r.used) return false;
-        }
-        return true;
-    }
-    
-    /*
-        한 번 더 풀어보자
-     */
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        Room[] room = new Room[N];
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            room[i] = new Room(a, b, false);
-        }
-
-        Arrays.sort(room, (a, b) -> {
-            if(a.end == b.end) return a.start - b.start;
-            return a.end - b.end;
-        });
 
         /*
+        0 40
         5 10
         15 30
-        0 40
+
+        10 40
+
+        15
          */
-
-            int total = 0;
-            int idx = 0;
-            for(int j=0; j<N; j++){
-                if(check(room)) break;
-                if(room[j].used) continue;
-
-                idx++;
-//                System.out.println(idx + "번 돔");
-
-                int count = 1;
-                int finish = room[j].end;
-                room[j].used = true;
-                for(int k=j+1; k<N; k++){
-                    if(finish <= room[k].start && !room[k].used){
-                        room[k].used = true;
-                        count++;
-                        finish = room[k].end;
-                    }
-                }
-                total += count;
-//                System.out.println((z-1) + "번 돔: " + total);
-                if(total == N) break;
+        int[][] arr = new int[N][2];
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<2; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
+        }
 
-            System.out.println(idx);
+        Arrays.sort(arr, (a, b) -> {
+            if(a[0] == b[0]) return a[1] - b[1];
+            return a[0] - b[0];
+        });
 
-            /*
+        int ans = 1;
 
-            1	10
-            2	3
-            3	4
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-             */
+        pq.add(arr[0][1]);
+
+        for(int i=1; i<N; i++){
+            if(!pq.isEmpty()){
+                if(pq.peek() <= arr[i][0]){
+                    pq.poll();
+                }else{
+                    ans++;
+                }
+                pq.add(arr[i][1]);
+            }
+        }
+
+        System.out.println(ans);
 
     }
 }
